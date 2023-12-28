8290751ebc601e548a9d3db12b883ef83203f126
Pirogov <pirogovde@mer.ci.nsu.ru>
2023-12-28 18:28:52 +0700

    implemented simple routing

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+use app\core\Application;
+
+$app = new Application();
+
+$app->router->get('/contact', function() {
+    return 'Hello world';
+});
+
+$app->run();
