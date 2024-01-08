13e5d4987d2cf6a332b612c8e3c8402177cac4b1
Pirogov <pirogovde@mer.ci.nsu.ru>
2024-01-08 13:11:00 +0700

    added RegisterModel class, changed registration form

---
+++ models/RegisterModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\models;
+
+class RegisterModel
+{
+    public string $firstName;
+    public string $lastName;
+    public string $login;
+    public string $password;
+    public string $password_confirm;
+}
--- views/register.php
+++ views/register.php
@@ -3,7 +3,10 @@
 </h1>
 
 <form action="/register" method="POST">
+    <input type="text" name="firstName" placeholder="Имя">
+    <input type="text" name="lastName" placeholder="Фамилия">
     <input type="text" name="login" placeholder="Логин">
     <input type="text" name="password" placeholder="Пароль">
+    <input type="text" name="password_confirm" placeholder="Подтвердите пароль">
     <button type="submit">Отправить</button>
 </form>
