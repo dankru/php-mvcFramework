dbf46725096c208a224bf3a8217106053ca72231
Pirogov <pirogovde@mer.ci.nsu.ru>
2024-01-08 14:24:04 +0700

    implemented validation

--- core/BaseModel.php
+++ core/BaseModel.php
@@ -13,7 +13,6 @@ abstract class BaseModel
 
     public function loadData($data)
     {
-        var_dump(BaseModel::class);
         foreach ($data as $key => $value) {
             if (property_exists($this, $key)) {
                 $this->{$key} = $value;
@@ -22,8 +21,60 @@ abstract class BaseModel
     }
 
     abstract public function rules(): array;
+
+
+    public function addError(string $attribute, string $rule, $params = [])
+    {
+        $message = $this->errorMessages()[$rule] ?? '';
+        foreach ($params as $key => $value) {
+            $message = str_replace("{$key}", $value, $message);
+        }
+        $this->errors[$attribute][] = $message;
+    }
+
+    // Array for validation errors
+    public array $errors = [];
+
     public function validate()
     {
+        foreach ($this->rules() as $attribute => $rules) {
+            $value = $this->{$attribute};
 
+            foreach ($rules as $rule) {
+                $ruleName = $rule;
+                if (!is_string($ruleName)) {
+                    $ruleName = $rule[0];
+                }
+                if ($ruleName === self::RULE_REQUIRED && !$value) {
+                    $this->addError($attribute, self::RULE_REQUIRED);
+                }
+                if ($ruleName === self::RULE_EMAIL && !filter_var($value, FILTER_VALIDATE_EMAIL)) {
+                    $this->addError($attribute, self::RULE_EMAIL);
+                }
+                if ($ruleName === self::RULE_MIN && strlen($value) < $rule['min']) {
+                    $this->addError($attribute, self::RULE_MIN, $rule);
+                }
+                if ($ruleName === self::RULE_MAX && strlen($value) > $rule['max']) {
+                    $this->addError($attribute, self::RULE_MAX, $rule);
+                }
+                if ($ruleName === self::RULE_MATCH && $value !== $this->{$rule['match']}) {
+                    $this->addError($attribute, self::RULE_MATCH, $rule);
+                }
+            }
+        }
+
+        return empty($this->errors);
+    }
+
+    public function errorMessages()
+    {
+        return [
+            self::RULE_REQUIRED => 'This field is required',
+            self::RULE_EMAIL => 'This field must be valid email address',
+            self::RULE_MIN => 'Min length of this field must be {min}',
+            self::RULE_MAX => 'Max length of this field must be {max}',
+            self::RULE_MATCH => 'This field must be the same as {match}',
+            self::RULE_UNIQUE => 'A user with this {unique} already exists'
+        ];
     }
 }
