aa28022a0b0f5363f3d4da2045e2a1f270a1e5ab
Pirogov <pirogovde@mer.ci.nsu.ru>
2024-01-09 12:33:40 +0700

    added empty migration files

---
+++ migrations/m000001_initial.php
@@ -0,0 +1,14 @@
+<?php
+
+class m000001_initial
+{
+    public function up()
+    {
+        echo "applying migration";
+    }
+
+    public function down()
+    {
+        echo "migration down";
+    }
+}
