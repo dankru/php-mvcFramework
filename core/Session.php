4075bda5559f1533ae07869dac35e7acd20303e0
Pirogov <pirogovde@mer.ci.nsu.ru>
2024-01-11 19:09:45 +0700

    added session class

---
+++ core/Session.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\core;
+
+class Session
+{
+    protected const FLASH_KEY = 'flash_messages';
+
+    public function __construct()
+    {
+        session_start();
+        $flashMessages = $_SESSION[self::FLASH_KEY] ?? [];
+        // Pass array by reference
+        foreach ($flashMessages as $key => &$flashMessage) {
+            // Mark to be removed
+            $flashMessage['remove'] = true;
+        }
+        $_SESSION[self::FLASH_KEY] = $flashMessages;
+    }
+
+    public function setFlash($key, $message)
+    {
+        $_SESSION[self::FLASH_KEY][$key] = [
+            'remove' => false,
+            'value' => $message
+        ];
+
+    }
+
+    public function getFlash($key)
+    {
+        return $_SESSION[self::FLASH_KEY][$key]['value'] ?? false;
+    }
+
+    public function __destruct()
+    {
+
+        // Iterate over marked to be removed
+        $flashMessages = $_SESSION[self::FLASH_KEY] ?? [];
+        // Pass array by reference
+        foreach ($flashMessages as $key => &$flashMessage) {
+            // Remove marked
+            if ($flashMessage['remove']) {
+                unset($flashMessages[$key]);
+            }
+        }
+
+        $_SESSION[self::FLASH_KEY] = $flashMessages;
+    }
+}
